860fb2054c452e5d26022dc715371e8bca07dc06
Pietro Castelli <pietrocast03@gmail.com>
2025-06-27 12:01:14 +0200

    inizializzazione progetto

---
+++ setup.bat
@@ -0,0 +1,19 @@
+@echo off
+REM --- Crea environment virtuale Python in .venv ---
+python -m venv .venv
+
+REM --- Attiva l'ambiente ---
+call .venv\Scripts\activate.bat
+
+REM --- Aggiorna pip e installa le dipendenze ---
+pip install --upgrade pip
+pip install -r requirements.txt
+call .venv\Scripts\activate.bat
+
+echo.
+echo ==============================================
+echo L'ambiente virtuale è pronto e attivo!
+echo Per attivarlo in futuro: call .venv\Scripts\activate.bat
+echo Per avviare l'app: python backend\main.py
+echo ==============================================
+pause
