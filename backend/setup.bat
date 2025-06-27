322072137ba42fc311dfcd6663df86fa1601fbd2
Pietro Castelli <pietrocast03@gmail.com>
2025-06-27 16:26:58 +0200

    fix: update pip

--- backend/setup.bat
+++ backend/setup.bat
@@ -6,13 +6,11 @@ REM --- Attiva l'ambiente ---
 call .venv\Scripts\activate.bat
 
 REM --- Aggiorna pip e installa le dipendenze ---
-pip install --upgrade pip
+python -m pip install --upgrade pip
 pip install -r requirements.txt
-call .venv\Scripts\activate.bat
 
 echo.
 echo ==============================================
 echo L'ambiente virtuale è pronto e attivo!
 echo Per avviare l'app: python backend\main.py
 echo ==============================================
-pause
