3737a47290c88076fa60d111200ef9a98f9b6da8
Pietro Castelli <pietrocast03@gmail.com>
2025-06-30 14:19:27 +0200

    fix: usa route centralizzate

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import 'zone.js';
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import {
   HTTP_INTERCEPTORS,
   provideHttpClient,
@@ -9,20 +9,11 @@ import {
 } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
-import { LoginPageComponent } from './app/pages/login/login';
-import { WalletPageComponent } from './app/pages/wallet/wallet';
-import { AuthGuard } from './app/auth.guard';
 import { JwtInterceptor } from './app/jwt.interceptor';
-
-const routes: Routes = [
-  { path: 'login', component: LoginPageComponent },
-  { path: 'wallet', component: WalletPageComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: 'wallet', pathMatch: 'full' },
-];
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()), // supporto HttpClient + interceptor
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
